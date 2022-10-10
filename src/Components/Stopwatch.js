cbf3248c64d4f732ccaf0a0223fa4e3b4eb92099
Christian Darlington <bocacode@bocacodes-MacBook-Air.local>
2022-10-10 13:46:49 -0400

    timer is functional

--- src/Components/Stopwatch.js
+++ src/Components/Stopwatch.js
@@ -1,19 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const Stopwatch = () => {
 
   const [isRunning, setIsRunning] = useState(false)
+  const [elaspedTime, setElaspedTime] = useState(0)
   
+
+  useEffect(() => {
+    let id;
+     if (isRunning) {
+      id = setInterval(() => setElaspedTime(prevTime => prevTime + 1), 1000)
+     }
+     return () => {clearInterval(id)}
+  },[isRunning ])
 
   return ( 
      <div className="stopwatch">
       <h2>Stopwatch</h2>
-      <span className="stopwatch-time">0</span>
+      <span className="stopwatch-time">{elaspedTime}</span>
       <button onClick={() => setIsRunning((prevValue) => !prevValue)}>
         {isRunning ? 'Stop' : 'Start'}
         </button>
-      <button>
-        Stop
+      <button onClick={() =>  setElaspedTime(0)}>
+        Reset
         </button>
      </div>
    );
